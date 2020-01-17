6f2851d059a2f085be52593d3eb28c6fd91fe9e5
sboylan <44224149+sboylan@users.noreply.github.com>
2020-01-17 17:04:50 +0100

    canvas créé ouf !!
    j'ai créé un canvas.

---
+++ Assets/Scripts/TimeButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+ public class TimeButton : MonoBehaviour
+
+ {
+
+ void Start()
+  {
+      GetComponent<Button>().onClick.AddListener(TogglePause);
+     }
+
+ public void TogglePause() {
+         Time.timeScale = Mathf.Approximately(Time.timeScale, 0.0f) ? 1.0f : 0.0f;
+     }
+
+public void Accelerate()
+  {
+    Time.timeScale = Time.timeScale * 5.0f ;
+  }
+
+  public void SlowDown()
+    {
+      Time.timeScale = Time.timeScale / 5.0f ;
+    }
+
+ }
